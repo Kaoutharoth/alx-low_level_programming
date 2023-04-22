8615141574b0a12792b66266958ac8a13e7dd9fb
kaoutharoth <kaoutharoth@gmail.com>
2023-04-21 19:55:57 -0700

    hello world

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,12 +10,10 @@
 
 int main(void)
 {
-        int num;
+	int num;
 
-        for (num = 0; num < 10; num++)
-                putchar((num % 10) + '0');
-
-        putchar('\n');
-
-        return (0);
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+	putchar('\n');
+	return (0);
 }
--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,17 +2,17 @@
 
 /**
  * main - Prints numbers between 0 to 9 and letters between a to f.
- * 
+ *
  * Return: Always 0 (success)
 */
 int main(void)
 {
 int i;
-for (i=48; i<58; i++)
+for (i = 48; i < 58; i++)
 {
 putchar(i);
 }
-for(i=97; i<103; i++)
+for (i = 97; i < 103; i++)
 {
 putchar(i);
 }
