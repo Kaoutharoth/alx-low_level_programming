5f267d940f258c4d32d4afc08e3af88a4ee2ad41
kaoutharoth <kaoutharoth@gmail.com>
2023-04-26 15:04:10 -0700

    functions

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,31 +18,31 @@ void print_times_table(int n)
 				k = j;
 				if (j == 0)
 				{
-					putchar(k + '0');
+					_putchar(k + '0');
 				} else if (k < 10 && j != 0)
 				{
-					putchar(',');
-					putchar(' ');
-					putchar(' ');
-					putchar(' ');
-					putchar(k + '0');
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
 				} else if (k >= 10 && k < 100)
 				{
-					putchchar(',');
-					putchar(' ');
-					putchar(' ');
-					putchar((k / 10) + '0');
-					putchar((k % 10) + '0');
+					_putchchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((ik / 10) + '0');
+					_putchar((k % 10) + '0');
 				} else if (k >= 100)
 				{
-					putchar(',');
-					putchar(' ');
-					putchar((k / 100) + '0');
-					putchar(((k / 10) % 10) + '0');
-					putchar((k % 10) + '0');
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
 				}
 			}
-			putchar('\n');
+			_putchar('\n');
 		}
 	}
 }
