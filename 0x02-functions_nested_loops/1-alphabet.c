8d0e3f47f8e73e6af288a97e73bda3731387577e
kaoutharoth <kaoutharoth@gmail.com>
2023-04-25 18:01:40 -0700

    functions

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,10 +7,10 @@ void print_alphabet(void)
 {
         int i;
 
-        for (i = 97; i <= 122; i++)
-        {
-                _putchar(i);
-        }
-        _putchar('\n');
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
 }
 
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,19 +6,16 @@
  */
 void print_alphabet_x10(void)
 {
-        char j;
-        int i;
-
-        i = 0;
-
-        while (i < 10)
-        {
-                for (j = 97; j <= 122; j++)
-                {
-                        _putchar(j);
-                }
-                _putchar('\n');
-                i++;
-        }
+	char j;
+	int i;
+	i = 0;
+	while (i < 10)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+			putchar(j);
+		}
+		putchar('\n');
+		i++;
+	}
 }
-
