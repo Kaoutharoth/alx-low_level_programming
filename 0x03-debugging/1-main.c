4a75e06bb1590ae0311fa8c9269e6ce103b72113
kaoutharoth <kaoutharoth@gmail.com>
2023-04-27 07:57:40 -0700

    debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,12 +8,14 @@
 int main(void)
 {
 	int i;
+
 	printf("infinite loop incomig:(\n");
-	i=0;
+	i = 0;
 	/*while(i<10)*/
 	/*{*/
 	/* putchar(i);*/
 
-/*}*/printf("infinite loop avoided \\0/\n");
-return(0);
+/*}*/
+	printf("infinite loop avoided \\0/\n");
+return (0);
 }
