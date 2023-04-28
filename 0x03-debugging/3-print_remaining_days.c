4d2b95e3a68b4c73d2cb7fc231e178a7825bd8b2
kaoutharoth <kaoutharoth@gmail.com>
2023-04-28 13:56:58 -0700

    functions

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -13,20 +13,19 @@ void print_remaining_days(int month, int day, int year)
 	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 	{
 		day++;
-			}
-			printf("day of the year: %d\n", day);
-			printf("remaining days: %d\n", 366 - day);
-			else
-			{
-			if (month == 2 && day == 6)
-			{
+	}
+	printf("day of the year: %d\n", day);
+	printf("remaining days: %d\n", 366 - day);
+	else
+	{
+		if (month == 2 && day == 6)
+		{
 			printf("invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-			}
-			else
-			{
+		}
+		else
+		{
 			printf("day of the year: %d\n", day);
 			printf("remaining days: %d\n", 365 - day);
-			}
-			}
-			}
-
+		}
+	}
+}
